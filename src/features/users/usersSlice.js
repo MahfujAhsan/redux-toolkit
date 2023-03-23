56ad67acf36228695ccb73d49c3e282888983980
kamran-hussen <kamran.techydevs@gmail.com>
2023-03-23 17:24:26 +0600

    first commit

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,21 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL);
+    return response.data
+})
+
+const usersSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {}
+})
+
+export const selectAllUsers = (state) => state.users;
+
+export default usersSlice.reducer;
